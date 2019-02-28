69ab0c91a7f685e9e0cf7e185a10364b1470e5fd
Colin Bell <colin@dogoodercomics.co.uk>
2019-02-28 20:59:53 +0000

    adds lodash and day.js

---
+++ client/src/models/Fixture.js
@@ -0,0 +1,9 @@
+const Fixture = function(playerOne, playerTwo, fighterOne, fighterTwo, date){
+  this.playerOne = playerOne;
+  this.playerTwo = playerTwo;
+  this.fighterOne = fighterOne;
+  this.fighterTwo = fighterTwo;
+  this.date = date;
+}
+
+module.exports = Fixture;
---
+++ client/src/models/Player.js
@@ -0,0 +1,8 @@
+const Player = function(name){
+  this.name = name;
+  this.matchesWon = 0;
+  this.matchesLost = 0;
+  this.roundsWon = 0;
+}
+
+module.exports = Player;
