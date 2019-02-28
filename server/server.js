f17fc13b8408a1cf7ebd347c6cefc7bbc8052803
Colin Bell <colin@dogoodercomics.co.uk>
2019-02-28 16:33:53 +0000

    adds api for fixtures and players

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const cors = require("cors");
+
+app.use(cors())
+app.use(bodyParser.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('streetfighterleague');
+    const playersCollection = db.collection('players');
+    const fixturesCollection = db.collection('fixtures');
+    app.use('/api/players', createRouter(playersCollection));
+    app.use('/api/fixtures', createRouter(fixturesCollection));
+  })
+  .catch(console.error);
+
+app.listen(3000, function() {
+  console.log(`SFL server running on port ${this.address().port}`);
+})
